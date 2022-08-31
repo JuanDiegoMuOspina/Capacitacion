3ec1e1f4f912e0b7ddfd67ac00a749c1d4865659
Juan Diego <jdmunozo_1@uqvirtual.edu.co>
2022-08-30 19:21:22 -0500

    actualizar servicio

--- src/test/java/com/juan/clase/DemoMicroApplicationTests.java
+++ src/test/java/com/juan/clase/DemoMicroApplicationTests.java
@@ -1,13 +1,21 @@
 package com.juan.clase;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoMicroApplicationTests {
+	@Autowired
+	private ControllerCalculadora contro;
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void sumar1() {
+		contro.convertirDolaresPesos(500);
+		
 	}
 
 }
